37276b32fca053a7cbafebfc60eca81775997c3c
Mohamed Samy <m0hamedSamy>
2024-10-25 14:53:36 +0300

    Provide the app single dependencies.

--- app/src/main/java/com/example/mealzapp/di/AppModule.kt
+++ app/src/main/java/com/example/mealzapp/di/AppModule.kt
@@ -1,9 +1,25 @@
 package com.example.mealzapp.di
 
+import com.example.data.data_source.remote.MealsApi
+import com.example.data.data_source.remote.MealsRemoteDataSource
+import com.example.data.data_source.remote.MealsRemoteDataSourceImpl
+import com.example.data.data_source.remote.RetrofitFactory
+import com.example.data.repo.MealsRepositoryImpl
+import com.example.domain.repo.MealsRepository
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val appModule = module {
     single {
-
+        RetrofitFactory.createService(MealsApi::class.java)
+    }
+    single<MealsRemoteDataSource> {
+        MealsRemoteDataSourceImpl(get())
+    }
+    single<MealsRepository> {
+        MealsRepositoryImpl(get())
+    }
+    single {
+        Dispatchers.IO
     }
 }
