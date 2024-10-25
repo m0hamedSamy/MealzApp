ff3a020df18e054c13263cfd88251f25ae12b68a
Mohamed Samy <m0hamedSamy>
2024-10-25 14:57:49 +0300

    Add the entry point of the app.

--- app/src/main/java/com/example/mealzapp/MainActivity.kt
+++ app/src/main/java/com/example/mealzapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.mealzapp.navigation.MealzApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             com.example.ui.theme.MealzAppTheme {
-
+                MealzApp()
             }
         }
     }
