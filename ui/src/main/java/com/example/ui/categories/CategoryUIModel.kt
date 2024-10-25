7297953cb1970f61e5b3084cae07db1a46ffb97e
Mohamed Samy <m0hamedSamy>
2024-10-25 14:50:54 +0300

    Create the Categories ui model.

---
+++ ui/src/main/java/com/example/ui/categories/CategoryUIModel.kt
@@ -0,0 +1,8 @@
+package com.example.ui.categories
+
+data class CategoryUIModel(
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val imgUrl: String = ""
+)
